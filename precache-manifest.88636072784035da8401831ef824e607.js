00542d89d8d07034392538a5f3bdf50694d9cd37
qqqfoxr <qqqfoxr@gmail.com>
2020-10-14 19:42:33 +0800

    Updates

---
+++ precache-manifest.88636072784035da8401831ef824e607.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "607e99a52f6aa18c516bee0bf95ae254",
+    "url": "/1091-208410430-robofriends/index.html"
+  },
+  {
+    "revision": "e9c97b84de84f0d8d725",
+    "url": "/1091-208410430-robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "c359d74d8d4a120c5d31",
+    "url": "/1091-208410430-robofriends/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e9c97b84de84f0d8d725",
+    "url": "/1091-208410430-robofriends/static/js/2.0b5b9267.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/1091-208410430-robofriends/static/js/2.0b5b9267.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c359d74d8d4a120c5d31",
+    "url": "/1091-208410430-robofriends/static/js/main.dad2bed2.chunk.js"
+  },
+  {
+    "revision": "8405a628b9e7cb4c1cb8",
+    "url": "/1091-208410430-robofriends/static/js/runtime-main.42de9999.js"
+  }
+]);
